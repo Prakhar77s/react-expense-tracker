b88d44f69ed002ac57031611bbc316ae2ba4c787
Prakhar Singh <prakhars76@gmail.com>
2021-04-21 22:23:56 +0530

    version 1.0

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Mobile Recharge",
+    amount: 10.12,
+    date: new Date(2020, 2, 14),
+  },
+  {
+    id: "e2",
+    title: "guitar wall mount",
+    amount: 79.49,
+    date: new Date(2021, 3, 29),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New guitar",
+    amount: 450,
+    date: new Date(2021, 3, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
